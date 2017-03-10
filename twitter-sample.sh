b6a83249c04b449be70fcbf213dae1e60a1ae2de
michaelvandael <m.r.n.van.dael@student.rug.nl>
2017-03-10 17:19:53 +0100

    Update twitter-sample.sh

--- twitter-sample.sh
+++ twitter-sample.sh
@@ -3,22 +3,19 @@
 # Michael van Dael
 # 10-03-2017
 
-echo 
-zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
+echo "These are the number of Tweets on 20170301:12"
+zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | 
+      /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
 echo
-
-#s1250523@karora:/net/corpora/twitter2/Tweets/2017/03$ zless 20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep [^'RT'] | wc -l
-#33783
-
-zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep [^'RT'] | wc -l
+echo "These are the number of unique Tweets on 20170301:12"
+zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | 
+      /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep [^'RT'] | wc -l
 echo
-
-#s1250523@karora:/net/corpora/twitter2/Tweets/2017/03$ zless 20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep ^'RT' | wc -l
-#6793
-
-zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep ^'RT' | wc -l
+echo "These are the number of unique ReTweets on 20170301:12"
+zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | 
+      /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep ^'RT' | wc -l
 echo
-
-zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep -v ^'RT' | head -20
-echo
+echo "These are the first 20 unique Tweets that are not ReTweets on 20170301:12"
+zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | 
+      /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep -v ^'RT' | head -20
 
