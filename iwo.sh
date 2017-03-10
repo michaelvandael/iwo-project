9586e24044d7785f412f7b1e48e2929186915a5a
michaelvandael <m.r.n.van.dael@student.rug.nl>
2017-03-10 16:42:05 +0100

    Update iwo.sh

--- iwo.sh
+++ iwo.sh
@@ -3,22 +3,10 @@
 # Michael van Dael
 # 10-03-2017
 
-
-
-
-
-
-
 #s1250523@karora:/net/corpora/twitter2/Tweets/2017/03$ zless 20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
 #39377
-zless 20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
+zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
 echo
-
-
-
-
-
-
 
 #s1250523@karora:/net/corpora/twitter2/Tweets/2017/03$ zless 20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep [^'RT'] | wc -l
 #33783
@@ -32,6 +20,6 @@ echo
 zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep ^'RT' | wc -l
 echo
 
-zless/net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep -v ^'RT' | head -20
+zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text | sort -u | grep -v ^'RT' | head -20
 echo
 
