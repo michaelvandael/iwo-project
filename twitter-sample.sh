dec80e73c6baf38bb9ac7b75cd864f0331aa789a
michaelvandael <m.r.n.van.dael@student.rug.nl>
2017-03-10 17:08:51 +0100

    Update and rename iwo.sh to twitter-sample.sh

--- twitter-sample.sh
+++ twitter-sample.sh
@@ -3,8 +3,7 @@
 # Michael van Dael
 # 10-03-2017
 
-#s1250523@karora:/net/corpora/twitter2/Tweets/2017/03$ zless 20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
-#39377
+echo 
 zless /net/corpora/twitter2/Tweets/2017/03/20170301:12.out.gz | /net/corpora/twitter2/tools/tweet2tab -i text user | wc -l 
 echo
 
